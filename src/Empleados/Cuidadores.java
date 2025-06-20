e880747f34ad0e50b2e4f9e6160a120f96ed8501
Mapacho <duvanlopez1407@gmail.com>
2025-06-20 16:55:31 -0600

    Clase Cuidadores
    Se creo la Clase hija cuidadores con sus metodos get, set , constructor, funciones y toString

---
+++ src/Empleados/Cuidadores.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Empleados;
+
+import java.time.LocalDate;
+import Animales.Animal;
+/**
+ *
+ * @author Student
+ */
+public class Cuidadores extends Empleado {
+    private String Especialidad;
+    private String[] RegistroActividades;
+
+    public String getEspecialidad() {
+        return Especialidad;
+    }
+
+    public String[] getRegistroActividades() {
+        return RegistroActividades;
+    }
+
+    
+    public void setEspecialidad(String Especialidad) {
+        this.Especialidad = Especialidad;
+    }
+    
+    public void Alimentar(Animal idAnimal){
+        for(int i=0; i<155;i++){
+            if(RegistroActividades[i]==null){
+                RegistroActividades[i]= "Se alimentó: " + idAnimal;
+                break;
+            }
+        }
+    }
+    
+    public void VerEstadoSalud (Animal idAnimal){
+        for(int i=0; i<155;i++){
+            if(RegistroActividades[i]==null){
+                RegistroActividades[i]= "Se vió el estado del: " + idAnimal;
+                break;
+            }
+        }
+    }
+            
+    public Cuidadores( int cedula, String NombreComp, LocalDate FechaNacimiento, String Telefono, double SalarioMen,String Especialidad) {
+        super(cedula, NombreComp, FechaNacimiento, Telefono, SalarioMen);
+        this.Especialidad = Especialidad;
+        this.RegistroActividades = new String[156];
+    }
+    
+    
+}
