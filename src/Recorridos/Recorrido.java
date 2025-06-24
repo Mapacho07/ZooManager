f1f1be7f9f58c7ecd5377c50e8ea1aa159a5bd4a
Mapacho <duvanlopez1407@gmail.com>
2025-06-24 04:50:00 -0600

    Clase Recorrido
    Se crea la clase recorrido sus get, constructor y método necesario

---
+++ src/Recorridos/Recorrido.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Recorridos;
+
+import Empleados.Guías;
+import java.time.LocalDate;
+import Visitantes.Visitante;
+import Animales.Animal;
+/**
+ *
+ * @author duvan
+ */
+public class Recorrido {
+    private Guías Guia;
+    private LocalDate Fecha;
+    private Visitante visitantes[];
+    private Animal Animales[];
+
+    public Guías getGuia() {
+        return Guia;
+    }
+
+    public LocalDate getFecha() {
+        return Fecha;
+    }
+
+    public Visitante[] getVisitantes() {
+        return visitantes;
+    }
+
+    public Animal[] getAnimales() {
+        return Animales;
+    }
+
+    public void Añadiranimalvist(Animal idAnimal){
+        for (int i = 0; i < 50; i++){
+            if (Animales[i]==null){
+                Animales[i]=idAnimal;
+                break;
+            }
+        }
+    }
+
+    public Recorrido(Guías Guia) {
+        this.Guia = Guia;
+        this.Fecha = Fecha;
+        this.visitantes = visitantes;
+        this.Animales = this.Animales = new Animal[50];
+    }
+    
+    
+    
+    
+}
